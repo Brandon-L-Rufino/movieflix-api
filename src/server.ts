7e6a4b8f0dddf9729fd517eb208a2d12dcca5615
Brandon Lopes <brandon_lopes@hotmail.com>
2025-09-30 20:44:59 -0300

    adicionando ordenação, gênero e lingua do filme

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,16 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/movies", async (req, res) => {
-    const movies = await prisma.movie.findMany();
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc"
+        },
+        include: {
+            genres: true,
+            languages: true,
+        }
+        
+    });
     res.json(movies); // agora estou buscando os filmes da tabela movies, o retorno da nossa API sera um json com todos os filmes
 });
 
